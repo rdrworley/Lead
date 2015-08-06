4a4f1947ae3a10976af9c1423f05cedd21062987
unknown <idz7@L309348.cdc.gov>
2015-08-06 15:49:12 -0400

    From Work (8/6/2015)

---
+++ AALMv4.1/AALM_LG_OUT_v4_1_040915.m
@@ -0,0 +1,45 @@
+%--------------------------------------------------------------------
+%
+% File: AALM_LG_OUT.m
+%
+% Created by Gary Diamond, SRC Inc, 04/09/15
+%
+%This run time file writes output from AALM_LG.csl to AALM_LG_OUTPUTDATA.DAT
+% --------------------------------------------------------------------
+
+a=[_day];
+b=[_year];
+c=[_intake];
+d=[_cblood];
+e=[_cplas];
+%f=[_crbc];
+f=[_ckidney];
+g=[_cliver];
+h=[_ccort];
+k=[_ctrab];
+l=[_cbone];
+m=[_ablood];
+n=[_aplas];
+o=[_arbc];
+p=[_akidney]; 
+q=[_aliver];
+r=[_acort];
+s=[_atrab];
+t=[_abone];
+u=[_asoft];
+v=[_abrain];
+w=[_art];
+x=[_aurine];
+y=[_afecal];
+z=[_tbody];
+za=[_pclear];
+zb=[_bclear];
+
+
+outputmatrix=[a,b,c,d,e,f,g,h,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,za,zb];
+outmatrix=rot90(fliplr(outputmatrix));
+out=fopen('AALM_LG_OUTPUTDATA.DAT','w');
+fprintf(out,"%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",outmatrix);
+fclose(out);
+
+
